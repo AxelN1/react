6a38ffcc1f1c773c973907cce5f510d1b37859d4
Axel <anamujimbo@gmail.com>
2021-04-07 16:13:59 +0200

    'React'

---
+++ chapitre_01/test/src/components/Steps.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+class Steps extends React.component {
+
+    render() {
+        return (
+
+            <div>
+
+
+
+                
+            </div>
+
+
+
+        )
+
+
+
+    }
+
+
+
+
+}
+
+export default Steps
