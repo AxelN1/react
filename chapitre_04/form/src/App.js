60866d0fa93cd46be22626733d6bddeb0b216889
Axel <anamujimbo@gmail.com>
2021-04-13 15:25:19 +0200

    'React'

---
+++ chapitre_04/form/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+
+    <h1 className="text-center">Login</h1>
+    <input 
+
+    </div>
+  );
+}
+
+export default App;
