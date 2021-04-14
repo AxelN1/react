ab3980f43fcf50853cc52eb7c616c2da33baba96
Axel <anamujimbo@gmail.com>
2021-04-14 16:52:19 +0200

    'React'

---
+++ chapitre_04/bakery/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  constructor() {
+    super()
+
+    this.state = {
+      activeTab : "add ",
+      items : [] 
+    }
+
+  return (
+    
+  );
+}
+
+export default App;
