92a4f68c300d98c981bba4c65e19c4411acfead7
Axel <anamujimbo@gmail.com>
2021-04-15 17:26:32 +0200

    'React'

--- chapitre_04/bakery/src/App.js
+++ chapitre_04/bakery/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from './components/Button.jsx';
-import Add from './components/Add.jsx';
-import List from './components/List.jsx';
-import Pay from './components/Pay.jsx';
+// import Button from './components/Button.jsx';
+// import Add from './components/Add.jsx';
+// import List from './components/List.jsx';
+// import Pay from './components/Pay.jsx';
 
 
 class App extends React.Component {
@@ -24,6 +24,19 @@ class App extends React.Component {
     return (
 
       <div>
+
+      <nav className="col">
+          <p>
+            <a href="./components/Add">Add</a>
+          </p>
+          <p>
+            <a href="./components/List">List</a>
+          </p>
+          <p>
+            <a href="./components/Pay">Pay</a>
+          </p>
+
+        </nav>
 
       <h1>Bakery</h1>
 
