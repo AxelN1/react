cf38afaa1f89a0fc89e037b38028ed5d25858c5e
Axel <anamujimbo@gmail.com>
2021-05-17 17:18:01 +0200

    Moovice 2

--- chapitre_06/moovice/src/views/Favorites.jsx
+++ chapitre_06/moovice/src/views/Favorites.jsx
@@ -1,6 +1,39 @@
 import React, { Component } from 'react'
 
 class Favorites extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            movies: [],
+            favIds: this.getStorage
+        }
+    }
+
+
+    componentDidMount() {
+        const arrFavIDs = this.state.favIDs;
+        arrFavIDs.map((elem) => {
+          let movie = this.getMovie(elem);
+          return movie;
+        });
+      }
+
+
+    getStorage() {
+
+        return Favorites
+    }
+
+    getMovie = (id) => {
+        console.log('id', id);
+        fetch(
+          `http://api.themoviedb.org/3/movie/${id}?api_key=e441f8a3a151d588a4932d2c5d310769`
+        );
+      };
+
+
     render() {
         return (
             <div>
